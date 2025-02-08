779f066cfd939a157f1f1dbed2d69a237802ac24
DuduCreator <117525771+DuduCreator@users.noreply.github.com>
2025-02-08 13:22:43 +0000

    edited some things

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import BlogList from './components/BlogList';
-import BlogPost from './components/BlogPost';
-import CreatePost from './components/CreatePost';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<BlogList />} />
-            <Route path="/post/:id" element={<BlogPost />} />
-            <Route path="/create" element={<CreatePost />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-export default App; 
+export default App;
